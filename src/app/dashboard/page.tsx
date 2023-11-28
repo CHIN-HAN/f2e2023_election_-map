47e29625a7917060b2bc6acca0bccecab77b8a45
pg <frankpg267@gmail.com>
2023-11-28 08:27:40 +0800

    feat: add map event and style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
 import Map from '@/components/map/map';
 export default function Home() {
   return (
-    <div className="w-3/5 h-screen pt-[100px]">
-      <Map></Map>
+    <div className="md:w-3/5 w-full md:h-screen h-[324px] pt-[100px]">
+      <Map />
     </div>
   );
 }
